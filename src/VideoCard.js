fecfb08c2ecae099c9a5e01e82bcbf48925168b4
sumanitian <sumanprasad26042001@gmail.com>
2021-07-15 10:19:48 +0530

    commit

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './VideoCard.css';
 
 function VideoCard() {
     const [isVideoPlaying, setIsVideoPlaying] = 
-    useState(false)
-    // useState
-    // useRef
+    useState(false);
+    const videoRef = useRef(null);
+
+        const onVideoPress = () => {
+            if (isVideoPlaying) {
+                //stop
+            } else {
+                // play
+            }
+        }
     return (
         <div className='videoCard'>
             <video
+                ref={videoRef}
+                onclick={onVideoPress}
                 className="videoCard__player"
                 src='https://www.instagram.com/reel/CRTLdt3DxyR/?utm_source=ig_web_button_share_sheet'
                 alt='IG reel video'
