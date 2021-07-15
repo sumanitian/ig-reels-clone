7bce55f07c1cf1128f83e014962a8ae7fc742188
sumanitian <sumanprasad26042001@gmail.com>
2021-07-15 10:07:06 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -9,12 +9,9 @@ function App() {
         <img className="app__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png" alt="">
         </img>
         <h1>Reels</h1>
-          {/* image at top-logo */}
-          {/* Reels text */}
       </div>
       <div className="app__videos">
-        {/* containeer of app_videos (scrollable container) *}
-        {/* <video> */}
+        <VideoCard />
         <VideoCard />
       </div>
     </div>
--- src/VideoCard.js
+++ src/VideoCard.js
@@ -6,7 +6,7 @@ function VideoCard() {
         <div className='videoCard'>
             <video
                 className="video__player"
-                src='https://instagram.flhr3-2.fna.fbcdn.net/v/t50.2886-16/117598442_205338364335864_545695080903278566_n.mp4?_nc_ht=instagram.flhr3-2.fna.fbcdn.net&_nc_cat=100&_nc_ohc=vjDPPklLnOYAX-xOjlv&oe=5F38FACF&oh=fbe19a529394ac8cc1a61f1b2b016e54'
+                src='https://www.instagram.com/reel/CRTLdt3DxyR/?utm_source=ig_web_button_share_sheet'
                 alt='IG reel video'
                 loop
             />
@@ -14,4 +14,4 @@ function VideoCard() {
     )
 }
 
-export default VideoCard
+export default VideoCard;
