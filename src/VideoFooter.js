5cebda99745fa0c4f8b5df2a568bbd607f223d73
sumanitian <sumanprasad26042001@gmail.com>
2021-07-15 12:41:10 +0530

    actionsleftright

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -3,30 +3,46 @@ import './VideoFooter.css'
 import { Avatar, Button } from '@material-ui/core';
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
 import Ticker from "react-ticker";
+import FavouriteIcon from '@material-ui/icons/Favorite';
+import ModeCommentIcon from '@material-ui/icons/ModeComment';
+import SendIcon from '@material-ui/icons/Send';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 function VideoFooter({ channel, song, likes, shares, avatarSrc }) {
     return (
         <div className='videoFooter'>
             <div className='videoFooter__text'>
-            <Avatar src={avatarSrc} />
-            <h3>
-                {channel} . <Button>Follow</Button>
-            </h3>
+                <Avatar src={avatarSrc} />
+                <h3>
+                    {channel} . <Button>Follow</Button>
+                </h3>
             </div>
 
             <div className="videoFooter__ticker">
-            <MusicNoteIcon
-            className ="videoFooter__icon"/> 
-            < Ticker mode = "smooth">
-                {
-                    ({index})=> (
-                <>
-                  
-                    <h1>{song}</h1>   
-                    
-                </>
-                    )}
-            </Ticker>
+                <MusicNoteIcon
+                    className="videoFooter__icon" />
+                < Ticker mode="smooth">
+                    {
+                        ({ index }) => (
+                            <>
+
+                                <h1>{song}</h1>
+
+                            </>
+                        )}
+                </Ticker>
+            </div>
+            <div className='videoFooter__actions'>
+                <div className='videoFooter__actionsLeft'>
+                    <FavouriteIcon fontsize="large" />
+                    <ModeCommentIcon fontsize="large" />
+                    <SendIcon fontsize="large" />
+                    <MoreHorizIcon fontsize="large" />
+                </div>
+                <div className='videoFooter__actionsRight'>
+                    <ModeCommentIcon />
+                    <p>{shares}</p>
+                </div>
             </div>
         </div>
     )
