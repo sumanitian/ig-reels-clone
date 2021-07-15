e726a8282946bda5900906e56a609a59cd910915
sumanitian <sumanprasad26042001@gmail.com>
2021-07-15 11:35:55 +0530

    added materialui and icons

--- src/VideoHeader.js
+++ src/VideoHeader.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './VideoHeader.css'
 function VideoHeader() {
     return (
-        <div>
+        <div className='videoHeader'>
             
         </div>
     )
