469a7f03d7aee2b9eb0f1bd1714e2e859248593b
sumanitian <sumanprasad26042001@gmail.com>
2021-07-15 09:46:52 +0530

    commit

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './VideoCard.css'
+import './VideoCard.css';
+
 function VideoCard() {
     return (
         <div className='videoCard'>
-            <video src='https://www.instagram.com/reel/CRTjvXIFvhF/?utm_source=ig_web_copy_link' alt='IG reel video' loop={true}>
-
-            </video>
+            <video
+                src='https://www.instagram.com/reel/CRTjvXIFvhF/?utm_source=ig_web_copy_link'
+                alt='IG reel video'
+                loop
+            />
         </div>
     )
 }
