816e2f7e1d4ff3f6632fda1e91bc3d2827b5e652
sumanitian <sumanprasad26042001@gmail.com>
2021-07-15 12:25:28 +0530

    musicticker

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './VideoFooter.css'
 import { Avatar, Button } from '@material-ui/core';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
 
 function VideoFooter({ channel, song, likes, shares, avatarSrc }) {
     return (
         <div className='videoFooter'>
+            <div className='videoFooter__text'>
             <Avatar src={avatarSrc} />
+            <h3>
+                {channel} . <Button>Follow</Button>
+            </h3>
+            </div>
+
+            <div className="videoFooter__ticker">
+            <MusicNoteIcon
+            className ="videoFooter__icon"/> 
+            </div>
         </div>
     )
 }
