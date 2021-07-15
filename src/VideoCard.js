1250152da1dadc716d8985f0b5ca22173f412158
sumanitian <sumanprasad26042001@gmail.com>
2021-07-15 09:57:23 +0530

    commit

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -5,7 +5,8 @@ function VideoCard() {
     return (
         <div className='videoCard'>
             <video
-                src='https://www.instagram.com/reel/CRTjvXIFvhF/?utm_source=ig_web_copy_link'
+                className="video__player"
+                src='https://instagram.flhr3-2.fna.fbcdn.net/v/t50.2886-16/117598442_205338364335864_545695080903278566_n.mp4?_nc_ht=instagram.flhr3-2.fna.fbcdn.net&_nc_cat=100&_nc_ohc=vjDPPklLnOYAX-xOjlv&oe=5F38FACF&oh=fbe19a529394ac8cc1a61f1b2b016e54'
                 alt='IG reel video'
                 loop
             />
