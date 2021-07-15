8c85a3feea89e552bc2d7ecaec75a6075b1a3842
sumanitian <sumanprasad26042001@gmail.com>
2021-07-15 09:20:38 +0530

    videoborder

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     // BEM naming convention
     <div className="app">
-      {/* <h1></h1> */}
       <div className="app__top">
         <img className="app__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png" alt="">
         </img>
