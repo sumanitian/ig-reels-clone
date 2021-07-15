1a04f0e58315a6d5f842549ac48f806da025ff1b
sumanitian <sumanprasad26042001@gmail.com>
2021-07-15 13:45:56 +0530

    mappedwithdatabase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import VideoCard from './VideoCard';
+import db from "./firebase";
+
 function App() {
+  const [reels, setReels] = useState([]);
+
+  useEffect(() => {
+    //App component will run once when it loads, and never again
+    db.collection('reels').onSnapshot(snapshot => (
+      setReels(snapshot.docs.map(doc => doc.data()))
+    ))
+  }, [])
+
+
   return (
     // BEM naming convention
     <div className="app">
@@ -11,16 +23,16 @@ function App() {
         <h1>Reels</h1>
       </div>
       <div className="app__videos">
-        <VideoCard 
-          channel='sumanitian'
-          avatarSrc='https://instagram.fixr3-1.fna.fbcdn.net/v/t51.2885-19/s150x150/130721200_3598634120231465_4726262765774510935_n.jpg?_nc_ht=instagram.fixr3-1.fna.fbcdn.net&_nc_ohc=mBhdRUR_3Y4AX8LKylc&edm=ABfd0MgBAAAA&ccb=7-4&oh=14745eb7e959d7e09cd7779bc578f631&oe=60F73C81&_nc_sid=7bff83'
-          song='test song - anupama_parameswaran'
-          url='https://www.instagram.com/reel/CRTjvXIFvhF/?utm_source=ig_web_copy_link'
-          likes={950}
-          shares={330}
-
-        />
-        <VideoCard />
+        {reels.map(({ channel, avatarSrc, song, url, likes, shares }) => (
+          <VideoCard
+            channel={channel}
+            avatarSrc={avatarSrc}
+            song={song}
+            url={url}
+            likes={likes}
+            shares={shares}
+          />
+        ))}
       </div>
     </div>
   );
