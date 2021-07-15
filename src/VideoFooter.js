ce4fd82f8fbc7e12745484e232a6baa4d03a286e
sumanitian <sumanprasad26042001@gmail.com>
2021-07-15 12:12:34 +0530

    imported avatar and sytled videofooter

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './VideoFooter.css'
+import { Avatar, Button } from '@material-ui/core';
+
 function VideoFooter({ channel, song, likes, shares, avatarSrc }) {
     return (
         <div className='videoFooter'>
-            
+            <Avatar src={avatarSrc} />
         </div>
     )
 }
