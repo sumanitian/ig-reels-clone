c46857c338d2e6e094d948c09919910d133e9b3d
sumanitian <sumanprasad26042001@gmail.com>
2021-07-15 12:02:24 +0530

    hardcoded the props

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ function App() {
       </div>
       <div className="app__videos">
         <VideoCard 
-          channel={channel}
-          avatarSrc={avatarSrc}
-          song={song}
-          url={url}
-          likes={likes}
-          shares={shares}
+          channel='sumanitian'
+          avatarSrc='https://instagram.fixr3-1.fna.fbcdn.net/v/t51.2885-19/s150x150/130721200_3598634120231465_4726262765774510935_n.jpg?_nc_ht=instagram.fixr3-1.fna.fbcdn.net&_nc_ohc=mBhdRUR_3Y4AX8LKylc&edm=ABfd0MgBAAAA&ccb=7-4&oh=14745eb7e959d7e09cd7779bc578f631&oe=60F73C81&_nc_sid=7bff83'
+          song='test song - anupama_parameswaran'
+          url='https://www.instagram.com/reel/CRTjvXIFvhF/?utm_source=ig_web_copy_link'
+          likes={950}
+          shares={330}
 
         />
         <VideoCard />
